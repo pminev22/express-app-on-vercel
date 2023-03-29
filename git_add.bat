04f3f448cc155784eb2ef8a9d9d48717bbc5b176
Petar Minev <peter_minev@abv.bg>
2023-03-29 18:42:31 +0300

    first commit

---
+++ git_add.bat
@@ -0,0 +1,7 @@
+echo "# express-app-on-vercel" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/pminev22/express-app-on-vercel.git
+git push -u origin main
